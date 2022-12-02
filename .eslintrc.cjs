dea25c1a8a2cb4238032addf95d6236e017dfc03
CherokiDev <miguelangelmorato84@gmail.com>
2022-12-02 19:07:17 +0100

    created basic structure, LayoutView, MainPage and HeaderComponent

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
 		sourceType: 'module',
 	},
 	plugins: ['react'],
-	rules: {},
+	rules: { 'react/prop-types': 'off' },
 	settings: {
 		react: {
 			version: 'detect',
---
+++ src/pages/main/MainPage.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+import LayoutView from '../../views/layout/LayoutView'
+
+const MainPage = () => {
+	return (
+		<LayoutView>
+			<Typography>página main</Typography>
+		</LayoutView>
+	)
+}
+
+export default MainPage
