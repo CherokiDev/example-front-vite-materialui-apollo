0d3041466b592cc17ae7d12a5e5f96b586141818
CherokiDev <miguelangelmorato84@gmail.com>
2022-12-06 15:43:54 +0100

    update query to connect with local backend

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,10 +1,35 @@
-import { Typography } from '@mui/material'
+import { useQuery } from '@apollo/client'
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableRow,
+	Typography,
+} from '@mui/material'
+import { GET_NAMES } from '../../queries/getNames'
 import LayoutView from '../../views/layout/LayoutView'
 
 const MainPage = () => {
+	const { data, loading, error } = useQuery(GET_NAMES)
+
+	console.log('data', data)
+
 	return (
 		<LayoutView>
-			<Typography>página main</Typography>
+			<Typography variant='h4'>Main Page</Typography>
+			{loading && <Typography>Loading...</Typography>}
+			{error && <Typography>Error: {error.message}</Typography>}
+			{data && (
+				<Table>
+					<TableBody>
+						{data.users.map((user, i) => (
+							<TableRow key={`${user.nombre}-${i}`}>
+								<TableCell>{user.nombre}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			)}
 		</LayoutView>
 	)
 }
